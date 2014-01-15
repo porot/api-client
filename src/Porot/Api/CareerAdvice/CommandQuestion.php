f35401707aa50587147de6b4f289e6307d049281
balkanski <emilian.dev@gmail.com>
2014-01-15 11:25:25 +0200

    added getQuestion command

--- src/Porot/Api/CareerAdvice/CommandQuestion.php
+++ src/Porot/Api/CareerAdvice/CommandQuestion.php
@@ -4,13 +4,13 @@ namespace Porot\Api\CareerAdvice;
 
 use Porot\Api\AbstractCommand;
 
-class CommandTopic extends AbstractCommand
+class CommandQuestion extends AbstractCommand
 {
     protected function build()
     {
         parent::build();
 
-        // Add topic id param to path
+        // Add question id param to path
         $path = $this->request->getPath();
         $path .= $this->data['id'];
         $this->request->setPath($path);
